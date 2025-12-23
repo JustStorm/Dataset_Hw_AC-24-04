bcd06ef10babb6be5127dd68887235bdbbd2c029
zdazzz <kiselevstas2007@gmail.com>
2025-12-24 01:07:24 +0300

    поиск(ред2)

--- substring_searcher.h
+++ substring_searcher.h
@@ -1,118 +1,83 @@
 #pragma once
-#ifndef SUBSTRING_SEARCHER_H
-#define SUBSTRING_SEARCHER_H
+#ifndef SUBSTRING_SEARCH_H
+#define SUBSTRING_SEARCH_H
 
 #include <vector>
 #include <string>
 #include <functional>
-#include <algorithm>
 
-class SubstringSearcher {
-private:
-    static std::vector<int> computeLPS(const std::string& pattern);
-    static int kmpSearch(const std::string& text, const std::string& pattern);
+template<typename DatasetType, typename Getter>
+std::vector<int> countSubstringOccurrences(
+    DatasetType& dataset,
+    Getter getter,
+    const std::string& pattern
+) {
+    int size = dataset.get_size();
+    std::vector<int> results(size, 0);
 
-public:
-    template<typename DatasetType>
-    static std::vector<int> searchInCity(DatasetType& dataset, const std::string& pattern) {
-        int size = dataset.get_size();
-        std::vector<int> results(size, 0);
-
-        for (int i = 0; i < size; i++) {
-            const char* fieldValue = dataset.get_city(i);
-            if (fieldValue != nullptr) {
-                std::string text(fieldValue);
-                std::string lowerText = text;
-                std::string lowerPattern = pattern;
-                std::transform(lowerText.begin(), lowerText.end(), lowerText.begin(), ::tolower);
-                std::transform(lowerPattern.begin(), lowerPattern.end(), lowerPattern.begin(), ::tolower);
-
-                results[i] = kmpSearch(lowerText, lowerPattern);
-            }
-        }
-
+    if (pattern.empty()) {
         return results;
     }
 
-    template<typename DatasetType>
-    static std::vector<int> searchInCounty(DatasetType& dataset, const std::string& pattern) {
-        int size = dataset.get_size();
-        std::vector<int> results(size, 0);
+    int m = pattern.length();
+    std::vector<int> lps(m, 0);
+    int len = 0;
+    int i = 1;
 
-        for (int i = 0; i < size; i++) {
-            const char* fieldValue = dataset.get_county(i);
-            if (fieldValue != nullptr) {
-                std::string text(fieldValue);
-                std::string lowerText = text;
-                std::string lowerPattern = pattern;
-                std::transform(lowerText.begin(), lowerText.end(), lowerText.begin(), ::tolower);
-                std::transform(lowerPattern.begin(), lowerPattern.end(), lowerPattern.begin(), ::tolower);
-
-                results[i] = kmpSearch(lowerText, lowerPattern);
+    while (i < m) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else {
+            if (len != 0) {
+                len = lps[len - 1];
+            }
+            else {
+                lps[i] = 0;
+                i++;
             }
         }
-
-        return results;
     }
 
-    template<typename DatasetType>
-    static std::vector<int> searchInState(DatasetType& dataset, const std::string& pattern) {
-        int size = dataset.get_size();
-        std::vector<int> results(size, 0);
+    for (int idx = 0; idx < size; idx++) {
+        const char* fieldValue = getter(dataset, idx);
+        if (fieldValue != nullptr) {
+            std::string text(fieldValue);
 
-        for (int i = 0; i < size; i++) {
-            const char* fieldValue = dataset.get_state(i);
-            if (fieldValue != nullptr) {
-                std::string text(fieldValue);
-                std::string lowerText = text;
-                std::string lowerPattern = pattern;
-                std::transform(lowerText.begin(), lowerText.end(), lowerText.begin(), ::tolower);
-                std::transform(lowerPattern.begin(), lowerPattern.end(), lowerPattern.begin(), ::tolower);
+            int n = text.length();
+            if (m <= n) {
+                int count = 0;
+                int textIdx = 0;
+                int patternIdx = 0;
 
-                results[i] = kmpSearch(lowerText, lowerPattern);
+                while (textIdx < n) {
+                    if (pattern[patternIdx] == text[textIdx]) {
+                        textIdx++;
+                        patternIdx++;
+                    }
+
+                    if (patternIdx == m) {
+                        count++;
+                        patternIdx = lps[patternIdx - 1];
+                    }
+                    else if (textIdx < n && pattern[patternIdx] != text[textIdx]) {
+                        if (patternIdx != 0) {
+                            patternIdx = lps[patternIdx - 1];
+                        }
+                        else {
+                            textIdx++;
+                        }
+                    }
+                }
+
+                results[idx] = count;
             }
         }
-
-        return results;
     }
 
-    template<typename DatasetType>
-    static std::vector<int> searchInWeatherCondition(DatasetType& dataset, const std::string& pattern) {
-        int size = dataset.get_size();
-        std::vector<int> results(size, 0);
-
-        for (int i = 0; i < size; i++) {
-            const char* fieldValue = dataset.get_weather_condition(i);
-            if (fieldValue != nullptr) {
-                std::string text(fieldValue);
-                std::string lowerText = text;
-                std::string lowerPattern = pattern;
-                std::transform(lowerText.begin(), lowerText.end(), lowerText.begin(), ::tolower);
-                std::transform(lowerPattern.begin(), lowerPattern.end(), lowerPattern.begin(), ::tolower);
-
-                results[i] = kmpSearch(lowerText, lowerPattern);
-            }
-        }
-
-        return results;
-    }
-
-    template<typename DatasetType>
-    static std::vector<int> searchInAllStringFields(DatasetType& dataset, const std::string& pattern) {
-        int size = dataset.get_size();
-        std::vector<int> totalResults(size, 0);
-
-        std::vector<int> cityResults = searchInCity(dataset, pattern);
-        std::vector<int> countyResults = searchInCounty(dataset, pattern);
-        std::vector<int> stateResults = searchInState(dataset, pattern);
-        std::vector<int> weatherResults = searchInWeatherCondition(dataset, pattern);
-
-        for (int i = 0; i < size; i++) {
-            totalResults[i] = cityResults[i] + countyResults[i] + stateResults[i] + weatherResults[i];
-        }
-
-        return totalResults;
-    }
-};
+    return results;
+}
 
 #endif 
